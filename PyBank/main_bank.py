0d03a25b3e119c000cc42534b99889d779b602d9
irwinmier96 <43219821+irwinmier96@users.noreply.github.com>
2019-02-01 16:02:11 -0800

    Add files via upload
    Updated version. Was able to get the total sum.

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -2,13 +2,16 @@ import os
 
 import csv
 
-total_sum = []
-
 csvpath = os.path.join('budget_data.csv')
 
 with open(csvpath, newline = '') as csvfile:
 
     csvreader =csv.reader(csvfile, delimiter = ',')
+    
+    sum_profit = 0
+    sum_loss = 0
+    profit = 0
+    total_profit = 0
 
     for month in csvreader:
         #line_num is a built-in csv function that counts all the rows in 
@@ -17,13 +20,22 @@ with open(csvpath, newline = '') as csvfile:
         month = csvreader.line_num - 1
 
     print(f"Total Months: {month}")
+
+    #reset the file pointer the csvreader 
+    #is associated with so that you can iterate over it again.
+    csvfile.seek(0)
+    next(csvreader)
     
     for row in csvreader:
-        values = row.strip().split(',')
-        total_sum.append(int(values[1]))
-        total_values = sum(total_sum)
+        profit = int(row[1])
+        if profit > 0:
+            sum_profit = sum_profit + profit
+        elif profit < 0:
+            sum_loss = sum_loss + profit
+    
+    total_profit = sum_profit + sum_loss
+    average = total_profit/len(int(profit))
+    print(f"Total: ${total_profit}")
+    print(f"Average: ${average}")
 
-        print(total_sum)
-
-
-        
+   
