f551a8356b5e3a0d15b6ab993f4686d89ff6be23
irwinmier96 <43219821+irwinmier96@users.noreply.github.com>
2019-02-04 19:30:08 -0800

    Add files via upload
    Final version of the code.

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -4,14 +4,17 @@ import csv
 
 csvpath = os.path.join('budget_data.csv')
 
+total_sum = []
+
+profitLossAverage = []
+
+changeAmount = []
+
+sumChangeAmount = 0
+
 with open(csvpath, newline = '') as csvfile:
 
     csvreader =csv.reader(csvfile, delimiter = ',')
-    
-    sum_profit = 0
-    sum_loss = 0
-    profit = 0
-    total_profit = 0
 
     for month in csvreader:
         #line_num is a built-in csv function that counts all the rows in 
@@ -25,17 +28,47 @@ with open(csvpath, newline = '') as csvfile:
     #is associated with so that you can iterate over it again.
     csvfile.seek(0)
     next(csvreader)
+
+    increase_max = 0
+    increase_max_date = ""
+    decrease_max = 0
+    decrease_max_date = ""
+
+    before = 0
     
     for row in csvreader:
-        profit = int(row[1])
-        if profit > 0:
-            sum_profit = sum_profit + profit
-        elif profit < 0:
-            sum_loss = sum_loss + profit
-    
-    total_profit = sum_profit + sum_loss
-    average = total_profit/len(int(profit))
-    print(f"Total: ${total_profit}")
-    print(f"Average: ${average}")
+        #Finding the sum of all the rows in the file
+         total_sum.append(int(row[1]))
+        #Finding the average change of each rows in the file
+        #This formula helps in finding the greatest increase
+        #as well as the greatest decrease
+         changeAmount = int(row[1]) - int(before)
+        #Make a new list to put the values of the differences
+        #between the rows
+         profitLossAverage.append(row[1])
+         if changeAmount > int(increase_max):
+            increase_max_date = row[0]
+            increase_max = changeAmount
+         elif changeAmount < int(decrease_max):
+            decrease_max_date = row[0]
+            decrease_max = changeAmount
+         before = row[1]
+         
+    lengthofProfitLoss = len(profitLossAverage) - 1 
+    firstIndexProfitLoss = profitLossAverage[0]
+    #need to index lengthofProfitLoss because we want the value in that specific index, 
+    # which is the last value on the list
+    ProfitLossAvg = (int(profitLossAverage[int(lengthofProfitLoss)]) - int(firstIndexProfitLoss))/int(lengthofProfitLoss)
 
+
+ 
+
+    print(f"Total: ${sum(total_sum)}")
+    print(f"Average Change: {ProfitLossAvg}")
+    print(f"Greatest Increase: {increase_max_date} (${increase_max})")
+    print(f"Greatest Decrease: {decrease_max_date} (${decrease_max})")
+
+  
+
+  
    
